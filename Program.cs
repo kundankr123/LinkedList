baec5bf604a2ecfcccfb24179e90b8b8cc18c5dd
kundan <goswamikundan123@gmail.com>
2023-02-26 16:31:17 +0530

    Adding file which creates a linkedlist.

--- Program.cs
+++ Program.cs
@@ -4,7 +4,14 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Linked List programs.");
+            //Console.WriteLine("Welcome to Linked List programs.");
+
+
+            Linkedlist list = new Linkedlist();
+            list.Add(70);
+            list.Add(30);
+            list.Add(56);
+            list.PrintList();
         }   
     }
 }
