e6bf4b88411430d54ea7893829caae882f0a65e2
kundan <goswamikundan123@gmail.com>
2023-02-26 16:11:34 +0530

    Adding file which displays Greeting Message.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LinkedList
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Linked List programs.");
+        }   
+    }
+}
